62291818dda2f8d971e1bca24e7d199ad4fdb1e7
luis992002 <2051063615@e.tlu.edu.vn>
2024-09-23 03:36:51 +0700

    done Ex02

---
+++ Ex02/app/src/main/java/com/example/ex02/SubActivity.java
@@ -0,0 +1,41 @@
+package com.example.ex02;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SubActivity extends AppCompatActivity {
+
+    public Button btn_BackToMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sub);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn_BackToMain = (Button) findViewById(R.id.btn_BackToMain);
+
+        btn_BackToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(SubActivity.this,MainActivity.class);
+                startActivity(back);
+            }
+        });
+
+    }
+}
