35f5b1ff13140d7dd0353e09b6253f110610a2ab
luis992002 <2051063615@e.tlu.edu.vn>
2024-10-05 14:50:29 +0700

    push Ex04 1/2

--- Ex04/app/src/main/java/com/example/ex04/MainActivity.java
+++ Ex04/app/src/main/java/com/example/ex04/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex04;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextFahrenheit;
+    private EditText editTextCelsius;
+    private Button buttonConvertToCelsius;
+    private Button buttonConvertToFahrenheit;
+    private Button buttonClear,buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,55 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextFahrenheit = findViewById(R.id.edtFahrenheit);
+        editTextCelsius = findViewById(R.id.edtCelsius);
+        buttonConvertToCelsius = findViewById(R.id.btnFC);
+        buttonConvertToFahrenheit = findViewById(R.id.btnCF);
+        buttonClear = findViewById(R.id.btnClear);
+        buttonNext = findViewById(R.id.btnNext);
+        buttonConvertToCelsius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fahrenheitString = editTextFahrenheit.getText().toString();
+                if (!fahrenheitString.isEmpty()) {
+                    double fahrenheit = Double.parseDouble(fahrenheitString);
+                    double celsius = (fahrenheit - 32) * 5 / 9;
+                    editTextCelsius.setText(String.valueOf(celsius));
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter a Fahrenheit value", Toast.LENGTH_SHORT).show();
+                }
+            }
+    });
+        buttonConvertToFahrenheit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String celsiusString = editTextCelsius.getText().toString();
+                if (!celsiusString.isEmpty()) {
+                    double celsius = Double.parseDouble(celsiusString);
+                    double fahrenheit = (celsius * 9 / 5) + 32;
+                    editTextFahrenheit.setText(String.valueOf(fahrenheit));
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter a Celsius value", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextFahrenheit.setText("");
+                editTextCelsius.setText("");
+            }
+        });
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent i = new Intent(MainActivity.this,BMIActivity.this);
+//                startActivity(i);
+            }
+        });
     }
 }
+
